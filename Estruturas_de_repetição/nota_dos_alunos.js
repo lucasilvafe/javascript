5d0ea136140fda8ff81c50fc3a6fe493aaeda385
lucasilvafe <luksilvafe@gmail.com>
2024-02-05 00:57:19 -0300

    Notas Dos Alunos Da Tarde

--- Estruturas_de_repetição/nota_dos_alunos.js
+++ Estruturas_de_repetição/nota_dos_alunos.js
@@ -1,13 +1,13 @@
 var notas = [
-    [['Lucas'], [10],[100],[600]],
-    [['Matheus1'], [5],[6],[2]],
-    [['Matheus2'], [5],[8],[3]],
-    [['Eduanea'], [0],[6],[7]],
-    [['Rodolfo'], [9],[5],[2]],
-    [['Suzana'], [5],[7],[5]],
-    [['Gustavo'], [5],[9],[0]],
-    [['Gabriel'], [7],[8],[9]],
-    [['Diego'], [5],[6],[4]]
+    [['Lucas'], [1],[5],[6]],
+    [['Matheus1'], [8],[6],[5]],
+    [['Matheus2'], [8],[8],[4]],
+    [['Eduanea'], [10],[6],[7]],
+    [['Rodolfo'], [9],[5],[5]],
+    [['Suzana'], [5],[7],[8]],
+    [['Gustavo'], [5],[9],[10]],
+    [['Gabriel'], [7],[7],[9]],
+    [['Diego'], [9],[5],[6]]
 ]
 //Armazena a quantidade de linhas
 var tamanhoLinhas = notas[0].length-1;
@@ -18,19 +18,47 @@ console.log('Comprimento da primeira linha é :' + tamanhoLinhas)
 console.log('Quantidade de colunas é: ' + tamanhoColunas)
 console.log('==================')
 
+//Este bloco varre o vetor notas
 function totalDeAlunos()
 {
-    var quantidadeAlunos =0
-    var posAluno = 0
+    var quantidadeAlunos = 0
     while (quantidadeAlunos < tamanhoColunas)
     {
         console.log('Notas')
         for (var linha = 0; linha <= tamanhoLinhas; linha++)
         {
-            console.log(notas[posAluno][linha])
+            console.log(notas[quantidadeAlunos][linha])
         }
         quantidadeAlunos++
-        posAluno++
     }   
 }
-return totalDeAlunos()
+//Aqui a função totalDeAlunos é chamada
+totalDeAlunos()
+
+console.log('============Resultado Final===============')
+
+function calculoNotaAluno()
+{
+for(var indexDoAluno = 0; indexDoAluno < tamanhoColunas; indexDoAluno++)
+{
+    var nota = 0;
+    for(var indexNota = 1; indexNota <= tamanhoLinhas; indexNota++)
+    {
+        nota = nota + Number(notas[indexDoAluno][indexNota])
+    }
+    var media = nota / tamanhoLinhas
+    var aprov;
+    console.log(`A soma das notas de ${notas[indexDoAluno][0]} foi: ${nota}`)
+    if (media > 6)
+    {
+        aprov = 'APROVADO'
+    }
+    else
+    {
+        aprov = 'REPROVADO'
+    }
+    console.log(`Com média de ${media.toFixed(1)} o aluno(a) está ${aprov}`)
+    console.log('')
+}
+}
+    calculoNotaAluno()
